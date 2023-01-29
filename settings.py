74149625c64df3c6127a122417a81098fb8a47a5
Clarissa G. Rodriguez <70369183+r-clarissa@users.noreply.github.com>
2023-01-29 22:23:59 +0800

    Updated documentation

--- settings.py
+++ settings.py
@@ -1,9 +1,3 @@
-
-'''
-    Author: Clarissa G. Rodriguez
-    Description: Accepts a list of instructions and outputs onscreen the pipelined version of processing those instructions.
-'''
-
 stageDesign = ['D', 'E', 'M', 'W'] 
 operations = ['add', 'mul', 'sub', 'div'] 
 registers = ['R0', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6', 'R7', 'R8', 'R9', 'R10', 'R11', 'R12', 'R13', 'R14', 'R15']
